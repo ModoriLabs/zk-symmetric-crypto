fa0f79c73f02353c4d8a331b373cca008aad7dd9
kyngwan@gmail.com <kyngwan@gmail.com>
2025-09-10 20:50:14 +0900

    chore: update hash for zk circuit

--- js/src/config.ts
+++ js/src/config.ts
@@ -6,7 +6,7 @@ import { webcrypto } from './webcrypto'
 const { subtle } = webcrypto
 
 // commit hash for this repo
-export const GIT_COMMIT_HASH = '2fcb282deb2b994a3ea4dd9039630ce2f94df8bf'
+export const GIT_COMMIT_HASH = 'f8df28a86369a6c8c0153c32892b7a991fe00122'
 
 export const CONFIG: { [E in EncryptionAlgorithm]: AlgorithmConfig } = {
 	'chacha20': {
