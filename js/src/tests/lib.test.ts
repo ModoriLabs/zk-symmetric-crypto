0405a22e59c78c1ca479083d30d55bb543d6e7fe
kyngwan@gmail.com <kyngwan@gmail.com>
2025-09-22 17:14:54 +0900

    chore: update supported algorithms for barretenberg

--- js/src/tests/lib.test.ts
+++ js/src/tests/lib.test.ts
@@ -28,9 +28,7 @@ const ALL_ALGOS: EncryptionAlgorithm[] = [
 ]
 
 const SUPPORTED_ALGO_MAP: { [T in ZKEngine]: EncryptionAlgorithm[] } = {
-	// TODO: impl more algos for barretenberg
-	// barretenberg: ['aes-256-ctr', 'aes-128-ctr'],
-	barretenberg: ['aes-128-ctr', 'chacha20'],
+	barretenberg: ['aes-128-ctr', 'aes-256-ctr', 'chacha20'],
 	expander: ['chacha20'],
 	gnark: ALL_ALGOS,
 	snarkjs: ALL_ALGOS,
